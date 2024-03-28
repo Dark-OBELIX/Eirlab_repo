f72860f7a2cb9b3cb93da7d47d7ee07bee761537
HLaplace <61471263+HLaplace@users.noreply.github.com>
2024-03-28 09:45:02 +0100

    Add files via upload

---
+++ 4_legs.ino
@@ -0,0 +1,39 @@
+ #include "FastLED.h"
+
+byte bright = 50; // luminosité des LEDs
+byte baza = 0;
+
+#define NUM_LEDS 150 // nombre de LEDs par bande
+#define L4 2 
+#define L3 4 
+#define L1 22  
+#define L2 23     
+CRGB leds1[NUM_LEDS];
+CRGB leds2[NUM_LEDS];
+CRGB leds3[NUM_LEDS];
+CRGB leds4[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812, L1, GRB>(leds1, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, L2, GRB>(leds2, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, L3, GRB>(leds3, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, L4, GRB>(leds4, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(bright);
+}
+void loop() {
+  int echelle_basse = 1;
+  int echelle_haute = 130;
+  colorStrip(echelle_basse, echelle_haute, L1, CRGB::Red); // Rouge
+  colorStrip(echelle_basse, echelle_haute, L2, CRGB::Red); // Rouge
+  colorStrip(echelle_basse, echelle_haute, L3, CRGB::Blue); // Rouge
+  colorStrip(echelle_basse, echelle_haute, L4, CRGB::Blue); // Rouge
+
+  }
+
+
+void colorStrip(int startIndex, int endIndex, int pin, CRGB color) {
+  for (int i = startIndex; i <= endIndex; i++) {
+    leds[i] = color; // Définir la couleur des LEDs
+  }
+  FastLED.show(); // Afficher les couleurs sur les LEDs
+}
