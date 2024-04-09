55469e932ee7d4ce0a550409616628420e5e28f6
HLaplace <61471263+HLaplace@users.noreply.github.com>
2024-04-09 10:07:22 +0200

    Update 4_legs.ino

--- 4_legs.ino
+++ 4_legs.ino
@@ -1,38 +1,46 @@
-#include "FastLED.h"
+   #include "FastLED.h"
 
 byte bright = 50; // luminosité des LEDs
 byte baza = 0;
 
 #define NUM_LEDS 150 // nombre de LEDs par bande
-#define L4 2 
-#define L3 4 
-#define L1 22  
-#define L2 23     
+#define PIN1 2  
+#define PIN2 4 
+#define PIN3 22  
+#define PIN4 23     
 CRGB leds1[NUM_LEDS];
 CRGB leds2[NUM_LEDS];
 CRGB leds3[NUM_LEDS];
 CRGB leds4[NUM_LEDS];
 
 void setup() {
-  FastLED.addLeds<WS2812, L1, GRB>(leds1, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<WS2812, L2, GRB>(leds2, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<WS2812, L3, GRB>(leds3, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  FastLED.addLeds<WS2812, L4, GRB>(leds4, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, PIN1, GRB>(leds1, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, PIN2, GRB>(leds2, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, PIN3, GRB>(leds3, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812, PIN4, GRB>(leds4, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(bright);
 }
 
 void loop() {
-  int echelle_basse = 0;
-  int echelle_haute = NUM_LEDS - 1;
-  colorStrip(echelle_basse, echelle_haute, leds1, CRGB::Red); // Rouge
-  colorStrip(echelle_basse, echelle_haute, leds2, CRGB::Red); // Rouge
-  colorStrip(echelle_basse, echelle_haute, leds3, CRGB::Blue); // Bleu
-  colorStrip(echelle_basse, echelle_haute, leds4, CRGB::Blue); // Bleu
-}
-
-void colorStrip(int startIndex, int endIndex, CRGB leds[], CRGB color) {
-  for (int i = startIndex; i <= endIndex; i++) {
-    leds[i] = color; // Définir la couleur des LEDs
-  }
-  FastLED.show(); // Afficher les couleurs sur les LEDs
-}
+    for (int i = 0; i < NUM_LEDS; i+=3) {
+      leds1[i] = CHSV(baza + i * 5, 255, 255);
+      leds1[i+1] = CHSV(baza + i * 5, 255, 255);
+      leds1[i+2] = CHSV(baza + i * 5, 255, 255);
+      
+      leds2[i] = CHSV(baza + i * 5, 255, 255);
+      leds2[i+1] = CHSV(baza + i * 5, 255, 255);
+      leds2[i+2] = CHSV(baza + i * 5, 255, 255);
+      
+      leds3[i] = CHSV(baza + i * 5, 255, 255);
+      leds3[i+1] = CHSV(baza + i * 5, 255, 255);
+      leds3[i+2] = CHSV(baza + i * 5, 255, 255);
+      
+      leds4[i] = CHSV(baza + i * 5, 255, 255);
+      leds4[i+1] = CHSV(baza + i * 5, 255, 255);
+      leds4[i+2] = CHSV(baza + i * 5, 255, 255);
+    }
+    baza++;
+    FastLED.setBrightness(bright);
+    FastLED.show();
+    //delay(500); 
+} 
